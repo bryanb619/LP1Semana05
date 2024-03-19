140545526c7541b5005daea6f5895ab8d1aca826
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-19 16:05:40 +0000

    Added crono1 start and 600 mileseconds sleep

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -8,16 +8,37 @@ namespace Chronos
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">Not Used</param>
         private static void Main(string[] args)
         {
 
-            // instance crono1 // instance of a class
+            // instances of a class
+
+            // instance crono1 
             Stopwatch crono1 = new Stopwatch(); 
 
             // instance crono2
             Stopwatch crono2 = new Stopwatch();
 
+
+
+
+            // start crono1
+            crono1.Start();
+
+            // sleep 0.6 seconds
+            Thread.Sleep(600); 
+
+        
+
+
+
            // Console.WriteLine("Hello, World!");
+
+
         }
     }
 }
