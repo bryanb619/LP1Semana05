2eb430da134fe74396b9501203be039da3f8963f
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-19 20:19:47 +0000

    Added RanDice to sln

---
+++ RanDice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
