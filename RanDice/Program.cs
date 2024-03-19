3e8cd3dab1ea8cbfa15aab12cae1b5005f4c8cf9
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-19 20:39:59 +0000

    Added full for logic

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,13 +6,36 @@ namespace RanDice
     {
         private static void Main(string[] args)
         {
-  
+
             // read arg 0 & convert to int
             int n = int.Parse(args[0]);
 
             // read arg 1 & convert to int
             int s = int.Parse(args[1]);
-            
+
+            Random r1 = new Random(s);
+            int diceResult = 0; 
+
+  
+
+            // loop until n times
+            for(int i = 0; i < n; i++)
+            {
+                // add random range for n number passed by user args[0]
+                int randomNumber = r1.Next(1, 6);
+
+                // === DEBUG ===
+                //Console.WriteLine(randomNumber); 
+                // dotnet run --project RanDice -- 3 334
+                diceResult += randomNumber;
+
+                // Reset randomNumber value
+                randomNumber = 0;  
+            }
+
+            Console.WriteLine(diceResult); 
+
+
 
         }
     }
