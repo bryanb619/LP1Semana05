a77dd894bcc4e58a5cdd47834fe3a5cffcacec2e
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-19 20:55:26 +0000

    Added constructor & Build

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,6 +2,20 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        // starting variables
+        private string name;
+        private float health;
+        private float shield; 
+
+
+
+        // constructor
+        public Enemy(string name)
+        {
+            this.name = name;   // Name
+            health = 100;       // Health
+            shield = 0;         // shield
+
+        }
     }
 }
