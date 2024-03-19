1668709c076b7fe8e351c9dd5724b7e58b1b4aaf
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-19 21:46:09 +0000

    Added enemy array

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,14 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            	
+
+            
+
+            Enemy[] enemies = new Enemy[];
+
+
+
+            /* TESTING CODE
             // creating instance
             Enemy enemy = new Enemy("Loki"); 
 
@@ -19,6 +26,10 @@ namespace MyGame
             // get name & health
             Console.WriteLine
             ($"{enemy.GetName()} | HP is: {enemy.GetHealth()}"); 
+
+            */ 
+
+
 
         }
     }
